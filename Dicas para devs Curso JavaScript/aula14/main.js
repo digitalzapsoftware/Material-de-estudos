51a5ae5af73aeaa622b8a4ed18e8b1ed4ccf3f29
Evanilson <programadornilson@gmail.com>
2022-05-21 15:40:06 -0300

    Novas aulas/ Dicas para devs!

--- Dicas para devs Curso JavaScript/aula14/main.js
+++ Dicas para devs Curso JavaScript/aula14/main.js
@@ -1,4 +1,16 @@
-function sam(a, b){
-    console.log(a + b)
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+       this.lastName = lastName;
+       this.age = age;
+    }
 
+    getFullName() {
+        console.log(`${this.firstName} ${this.lastName}`);
+    }
 }
+
+const person = new Person('Jane', 'Doe', 40)
+
+person.getFullName();
+
