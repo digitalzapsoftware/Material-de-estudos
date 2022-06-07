296d1408e91f0af70a0b1252ee1407970522bfe2
Evanilsonpg <99387388+Evanilsonpg@users.noreply.github.com>
2022-06-07 14:12:43 -0300

    novas aulas

--- curso-react-matheus-battisti/meu-projeto/src/App.js
+++ curso-react-matheus-battisti/meu-projeto/src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
-import Evento from './components/Evento';
-import Form from './components/Form';
+import Condicional from './components/Condicional';
 
 function App() {
 
   return (
 
     <div className="App">
-     <h1>Testando eventos</h1>
-     <Evento/>
-     <Form/>
+     <h1>Renderização Condicional</h1>
+     <Condicional/>
     </div>
 
   );
