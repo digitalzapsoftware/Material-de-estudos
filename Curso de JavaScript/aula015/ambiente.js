779c2dc97ad2fe09609e9342ea3a3e24ee54b8a0
Evanilsonpg <99387388+Evanilsonpg@users.noreply.github.com>
2022-06-08 17:18:50 -0300

    novas aulas

--- Curso de JavaScript/aula015/ambiente.js
+++ Curso de JavaScript/aula015/ambiente.js
@@ -3,6 +3,7 @@ let num = [5, 8, 2, 9, 3]
 num.push(1)
 num.sort()
 console.log(num)
+
 console.log(`O Vetor tem ${num.length} posiçoes`)
 
 console.log(`O Nosso primeiro valor do vetor é ${num[0]}`)
