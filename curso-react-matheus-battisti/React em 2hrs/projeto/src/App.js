9b5691c94285f39683d4145561a3e8a9705c596c
Evanilsonpg <99387388+Evanilsonpg@users.noreply.github.com>
2022-06-18 23:56:11 -0300

    novas aulas

--- curso-react-matheus-battisti/React em 2hrs/projeto/src/App.js
+++ curso-react-matheus-battisti/React em 2hrs/projeto/src/App.js
@@ -12,16 +12,24 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState('false')
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
        <div className='todo-header'>
          <h1>React Todo</h1>
        </div>
        <div className='form-todo'>
-         <p>Formulário</p>
+         <h2>Insira sua próxima tarefa:</h2>
+         <form onSubmit={handleSubmit}>
+
+         </form>
        </div>
        <div className='list-todo'>
-         <p>Lista</p>
+         <h2>Lista de tarefas</h2>
+         {todos.length === 0 && <p>Não há tarefas!</p>}
        </div>
 
     </div>
