c074133408fb5eebb1771420821718099fabd677
Evanilsonpg <99387388+Evanilsonpg@users.noreply.github.com>
2022-06-19 21:26:05 -0300

    NOVAS AULAS

--- curso-react-matheus-battisti/React em 2hrs/projeto/src/App.js
+++ curso-react-matheus-battisti/React em 2hrs/projeto/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import{useState, useEffect} from 'react'
-import{BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs'
+import{useState, useEffect} from 'react';
+import{BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs';
 
 const API = "https://localhost:5000";
 
@@ -13,26 +13,59 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time, 
+      done: false,
+    };
+
+    //Envio para API!!!
+
+    console.log('Enviou!')
+
+    setTitle('')
+    setTime('')
+
   }
 
   return (
+
     <div className="App">
+
        <div className='todo-header'>
          <h1>React Todo</h1>
        </div>
+
        <div className='form-todo'>
+
          <h2>Insira sua próxima tarefa:</h2>
          <form onSubmit={handleSubmit}>
+           <div className='form-control'>
+             <label htmlFor='title'>O que voce vai fazer?</label>
+             <input type="text" name='title' placeholder='Título da tarefa' onChange={(e) => setTitle(e.target.value)} value={title || ''} required></input>
+           </div>
 
+           <div className='form-control'>
+             <label htmlFor='time'>Duração:</label>
+             <input type="text" name='time' placeholder='Tempo estimado (em horas)' onChange={(e) => setTime(e.target.value)} value={time || ''} required></input>
+           </div>
+           <input type="submit" value="Criar tarefa"></input>
          </form>
+
        </div>
+
        <div className='list-todo'>
+
          <h2>Lista de tarefas</h2>
          {todos.length === 0 && <p>Não há tarefas!</p>}
+
        </div>
 
     </div>
-  );
+  )
 }
 
 export default App;
