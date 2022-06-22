04d59fb99b04e40265a38b498cf09e30b6ebe197
Evanilsonpg <99387388+Evanilsonpg@users.noreply.github.com>
2022-06-22 00:16:36 -0300

    novo projeto

--- encurtador-de-link/src/pages/Home/index.js
+++ encurtador-de-link/src/pages/Home/index.js
@@ -1,7 +1,26 @@
+import {FiLink} from 'react-icons/fi';
+import './home.css'
+
 export default function Home() {
     return(
-      <div>
-        <h1>Página Home!</h1>
+      <div className="container-home">
+        
+        <div className="logo">
+            <img src="/logo.png" alt="logo" />
+            <h1>Encurtador de Link</h1>
+            <span>Cole seu link para encurtar!</span>
+        </div>
+
+        <div className="area-input">
+            <div>
+                <FiLink size={24} color='#FFF' />
+                <input placeholder='Cole seu link aqui...'/>
+            </div>
+
+        <button>Encurtar Link</button>
+
+        </div>
+        
       </div>
     )
   }
--- encurtador-de-link/src/pages/Links/index.js
+++ encurtador-de-link/src/pages/Links/index.js
@@ -1,7 +1,25 @@
+import './links.css';
+import {FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi' 
+
+
 export default function Links() {
     return(
-      <div>
-        <h1>Página Links!</h1>
+      <div className='links-container'>
+        <div className='links-header'>
+            <FiArrowLeft size={34} color='#FFF' />
+            <h1>Meus Links</h1>
+        </div>
+
+      <div className='links-item'>
+        <button className='link'>
+          <FiLink size={14} color='#FFF'/>
+          https://sujeitoprogramador.com
+        </button>
+        <button>
+          <FiTrash  size={20} color='#FF5454'/>
+        </button>
+      </div>
+
       </div>
     )
   }
