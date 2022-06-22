5935501aee0dc81aa919b684cf5716113cf55302
Evanilsonpg <99387388+Evanilsonpg@users.noreply.github.com>
2022-06-21 23:14:57 -0300

    novas aulas

---
+++ encurtador-de-link/src/pages/Links/index.js
@@ -0,0 +1,7 @@
+export default function Links() {
+    return(
+      <div>
+        <h1>Página Links!</h1>
+      </div>
+    )
+  }
---
+++ encurtador-de-link/src/routes.js
@@ -0,0 +1,17 @@
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home'
+import Links from './pages/Links'
+
+function RoutesApp() {
+    return(
+        <BrowserRouter>
+            <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/links' element={<Links/>} />
+            </Routes>
+        </BrowserRouter>
+    )
+} 
+
+export default RoutesApp;
